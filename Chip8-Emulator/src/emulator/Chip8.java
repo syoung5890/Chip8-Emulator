0921687b9bd06d101cf71a76a1ef8dd96e694dbe
syoung5890 <76408517+syoung5890@users.noreply.github.com>
2022-02-10 18:23:33 -0500

    Layed out structure for chip8 class

--- Chip8-Emulator/src/emulator/Chip8.java
+++ Chip8-Emulator/src/emulator/Chip8.java
@@ -1,5 +1,53 @@
 package emulator;
 
 public class Chip8 {
-
+	
+	//constants
+	static final int MEM_SIZE = 0x1000;
+	static final int NUM_REGISTERS = 0x10;
+	static final int STACK_SIZE = 0x10;
+	
+	//memory 
+	private char[] mem;
+	private char[] V;
+	private char[] stack;
+	
+	private int I;
+	private char pc;
+	private char delayTimer;
+	private char soundTimer;
+	
+	public Chip8() {
+		mem = new char[MEM_SIZE];
+		V = new char[NUM_REGISTERS];
+		stack = new char[STACK_SIZE];
+	}
+	
+	public void decrementTimer() {
+		if(soundTimer != 0) {
+			soundTimer--;
+		}
+	}
+	
+	public void run() {
+		int instruction = fetch();
+		execute(instruction);
+	}
+	
+	private int fetch() {
+		int instruction;
+		instruction = (int)mem[pc];
+		instruction = (instruction<<8) + (int)mem[pc+1];
+		pc += 2;
+		return instruction;
+	}
+	
+	private void execute(int instruction) {
+		//rename this variable
+		String s = Integer.toHexString(instruction);
+		
+		switch(s.charAt(0)) {
+		
+		}
+	}
 }
